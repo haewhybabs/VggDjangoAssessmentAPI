0974ab00cc52bcafc32427fb5ce2a7c44b2de8e0
Haewhybabs <babalolaisaac@gmail.com>
2020-01-15 13:47:16 +0100

    migrations created

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name=models.CharField(max_length=50)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+class Action(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description=models.CharField(max_length=128)
+    notes=models.TextField()
+    completed=models.BooleanField(default=False)
+    
